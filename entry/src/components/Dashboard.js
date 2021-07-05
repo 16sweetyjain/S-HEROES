d578c2746365d6f85b56c387f2e76bd757790f4b
Anushka <anushka318singh@gmail.com>
2021-07-04 23:01:59 -0700

    signout added

--- entry/src/components/Dashboard.js
+++ entry/src/components/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { useHistory } from 'react-router';
 import MenstrualAnalysis from './DashboardCards/cards/MenstrualAnalysis';
 import BirthControl from './DashboardCards/cards/BirthControl';
 import ReproductiveHealth from './DashboardCards/cards/ReproductiveHealth';
@@ -7,22 +7,35 @@ import MentalHealth from './DashboardCards/cards/MentalHealth';
 import Awareness from './DashboardCards/cards/Awareness';
 import Reports from './DashboardCards/cards/Reports';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles({
     Heading: {
       fontSize: '40px',
       textAlign: 'center',
       margin: '20px'
-    }
+    },
+    Button: {
+        height: '30%',
+        width: '27%',
+        margin: 'auto',
+        display: 'block'
+      }
   });
 
 export default function Dashboard() {
     const classes = useStyles();
+    const history = useHistory();
+    
     return (
-        <div className='container'>
-            <div className={classes.Heading}>
-                Welcome
-            </div>
+        <div className='container row-wrap'>
+                <div className='row-w'>
+                    <div className={classes.Heading}>
+                            Welcome
+                    </div>
+                    <Button onClick={(() => history.push('/signin'))} className={classes.Button} size="small" variant="contained" color="primary">Signout</Button>
+                </div>
+                
             
             <div className='row'>
                 <div className='col s4'>
